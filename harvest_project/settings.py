fa93cce0c823820018a38fb22ad5e756c5b28d11
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-09-14 03:24:09 +0100

    allowed host modified

--- harvest_project/settings.py
+++ harvest_project/settings.py
@@ -28,11 +28,12 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = config(
-    "ALLOWED_HOSTS",
-    default="harvest-pro-yxv7.onrender.com,localhost,127.0.0.1",
-    cast=lambda v: [s.strip() for s in v.split(",")]
-)
+
+
+ALLOWED_HOSTS = [
+    "harvest-pro-yxv7.onrender.com",
+    "127.0.0.1",
+]
 
 
 # Application definition
